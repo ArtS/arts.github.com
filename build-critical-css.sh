4c163bbfab5935d5b55eb290fac93b31ddf6ac23
Artёm Skvira <art@nimblegecko.com>
2021-05-03 19:44:23 +1000

    New critical css

--- build-critical-css.sh
+++ build-critical-css.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # uses https://github.com/bezoerb/grunt-critical
-# cat ./_site/index.html | critical -b ./_site > ./_includes/critical.css
-cat ./_site/how-to-quickly-run-some-csharp-code-without-creating-a-project/index.html | critical -b ./_site > ./_includes/critical.css
+cat ./_site/index.html | critical -b ./_site > ./_includes/critical.css
+# cat ./_site/how-to-quickly-run-some-csharp-code-without-creating-a-project/index.html | critical -b ./_site > ./_includes/critical.css
